56e61c889fb9b5446edc19ba2fa7f94a4686f677
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2017-07-11 17:40:05 +0200

    created function which calculates local slopes by means of a differential quotient

--- irrigation/comp/slopes.py
+++ irrigation/comp/slopes.py
@@ -23,6 +23,23 @@ def slopes_movav(df):
                                       x)
     return df_slopes
 
+def local_slope(df, ref_col='datagap'):
+    """
+    Calculate slope by means of a differential quotient, e.g.
+    (y2-y1)/(x2-x1)
+    """
+    # calculate changes between observations
+    daily_diffs = transform.shift_diff(df, shift=1)
+    # calc diff qoutient by dividing each ts through the data gap
+    datagaps = daily_diffs[ref_col].values
+    daily_diffs = daily_diffs.drop(ref_col, axis=1)
+    # init empty df
+    df_slopes = pd.DataFrame(index=df.index)
+    for series in daily_diffs:
+        # divide changes in y by changes in x -> local slopes
+        df_slopes[series] = np.divide(daily_diffs[series], datagaps)
+    return df_slopes
+
 def slopes_climat(df):
     """operates only on climat"""
     # calculate slope using 1st and 3rd, then 2nd and 4th value
@@ -50,12 +67,6 @@ def psd(df, reference_index = 0):
     # return only positive values
     return df[df>0]
 
-def psds(df, start=0, end=366):
-    """"""
-    # TODO: temp function to test JJA psds
-    # return series object
-    return df[start:end].sum()
-
 def aggregate_psds(df, resampling='Q-NOV'):
     """Calculate aggregated sum"""
     return df.resample(resampling).sum()
@@ -67,18 +78,26 @@ if __name__=='__main__':
     import matplotlib.pyplot as plt
     from irrigation.prep import timeseries
 
-    gpi = 696365
+    gpi = 721798
     df = timeseries.prepare(gpi=gpi,
                             start_date='2007-01-01',
                             end_date='2011-12-31',
                             model='eraland',
                             satellites=['ascat','amsre'],
                             kind='movav')
-    df.plot()
-    # aggregate psds
-    df_slopes = slopes_movav(df)
-    df_psd = psd(df_slopes)
-    #seasonal = aggregate_psds(df_psd, 'Q-NOV')
-    #seasonal.plot(kind='bar')
+    # test the two slope functions
+    slopes_diff_quot = local_slope(df)
+    slopes_conv = slopes_movav(df)
+    # calc psd
+    psd_dq = psd(slopes_diff_quot)
+    psd_conv = psd(slopes_conv)
+    # aggregate to psds
+    psds_dq = aggregate_psds(psd_dq)
+    psds_conv = aggregate_psds(psd_conv)
 
+    print psds_conv
+    print psds_dq
+
+    psds_dq.plot(title='diffquot')
+    psds_conv.plot(title='conv')
     plt.show()
