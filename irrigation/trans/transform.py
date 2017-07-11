6781daf537bedf5c9e5e2e5c4c09ea6d9a9cfea6
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2017-07-11 17:40:53 +0200

    added function which calculates the difference between time series with an arbitrary shift

--- irrigation/trans/transform.py
+++ irrigation/trans/transform.py
@@ -7,4 +7,16 @@ def calc_gaps(df):
     df_gaps['date'] = df_gaps.index.values
     df_gaps['gaps'] = df_gaps.date.shift(1) - df_gaps.date
     df_gaps['gaps'] = df_gaps.gaps / np.timedelta64(1, 'D')
+    df_gaps.drop('date', axis=1, inplace=True)
     return df_gaps
+
+def shift_diff(df, shift=1):
+    """calc diff between df and df shifted by x days"""
+    df_copy = df.copy()
+    # bit dirty, initially holds dates, then the date gaps in days
+    df_copy['datagap'] = df.index.values
+    # (y_i - y_i-x) for each column
+    df_slopes = df_copy - df_copy.shift(shift)
+    df_slopes['datagap'] = df_slopes['datagap'] / np.timedelta64(1,'D')
+    return df_slopes
+
