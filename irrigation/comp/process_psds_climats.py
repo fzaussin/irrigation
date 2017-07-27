034eca34ddfedda7106e95d67e1f6928283283b7
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2017-07-27 17:32:31 +0200

    tailored to ascat jja climats, room for a lot of improvement!

--- irrigation/comp/process_psds_climats.py
+++ irrigation/comp/process_psds_climats.py
@@ -10,52 +10,53 @@ from irrigation.comp import slopes
 
 
 # define 1 model and multiple satellite datasets
-model = 'eraland'
-satellites = ['ascat', 'amsre']
+models = ['merra']
+satellites = ['ascat_reckless_rom']
 
 # path to gpis
 path = '/home/fzaussin/shares/users/Irrigation/Data/lookup-tables/LCMASK_rainfed_cropland_usa.csv'
 gpis_lcmask = pd.DataFrame.from_csv(path)
+gpis_lcmask = gpis_lcmask.sort_values('gpi_quarter')
 gpis = gpis_lcmask['gpi_quarter']
+total_gpis = len(gpis)
 
+# init results container
+results = []
 
 tic = time.clock()
-results = []
+counter = 0
 for gpi in gpis:
-    print gpi
+    counter += 1
+    print "Processing gpi #{} of {}".format(counter, total_gpis)
     # exemplary slopes calc from climats for JJA
     # 721798 is mississippi example gpi
     try:
         df = timeseries.prepare(gpi=gpi,
                                 start_date='2007-01-01',
-                                end_date='2011-12-31',
-                                model=model,
+                                end_date='2016-12-31',
+                                models=models,
                                 satellites=satellites,
-                                kind='movav')
-    except IOError:
+                                kind='clim')
+    except (IOError, ValueError):
         # skip gpi if no index for a location id can be found
         # problem with i=282 -> gpi #737696
         ascat = np.nan
-        amsre = np.nan
         continue
     try:
         climat_slopes = slopes.slopes_climat(df)
         psdiffs = slopes.psd(climat_slopes)
         # slopes for JJA climat
-        psdiffs_sum = slopes.psds(psdiffs, 151, 243)
-        # for each sat
-        amsre = psdiffs_sum[0]
-        ascat = psdiffs_sum[1]
+        psdiffs_sum = psdiffs[151:243].sum()
+        ascat = psdiffs_sum[0]
     except ValueError:
         # ValueError: if no data for gpi
         # IOError: if index for location id not found (gldas problem only?)
         ascat = np.nan
-        amsre = np.nan
 
-    results.append((gpi, ascat, amsre))
+    results.append((gpi, ascat))
 toc = time.clock()
 
-df_results = pd.DataFrame(results, columns=('gpi', 'ascat', 'amsre'))
-df_results.to_csv('/home/fzaussin/Desktop/TEST_USA_ascat_gldas_climat_psds_JJA.csv')
+df_results = pd.DataFrame(results, columns=('gpi', 'ascat'))
+df_results.to_csv('/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/usa-jja-climats-merra-ascatreckrom-200701-201612.csv')
 
 print "Elapsed time: ", str(datetime.timedelta(seconds=toc-tic))
