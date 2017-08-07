25672122ce365587d138233052415c14c9b24855
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2017-08-07 18:23:32 +0200

    added add_nan

--- irrigation/prep/timeseries.py
+++ irrigation/prep/timeseries.py
@@ -8,7 +8,7 @@ from irrigation.prep import interp, smooth
 # init data object
 data_object = importdata.QDEGdata()
 
-def prepare(gpi, start_date, end_date, models, satellites, kind="clim"):
+def prepare(gpi, start_date, end_date, models, satellites, kind="clim", window=35):
     """
     Prepare eraland, amsre, ascat, amsr2 time series for processing. First drop nan values,
     then calculate climatology or moving average as specified by kind and lastly
@@ -30,6 +30,7 @@ def prepare(gpi, start_date, end_date, models, satellites, kind="clim"):
     """
     # read data
     ts_input = data_object.read_gpi(gpi, start_date, end_date, models, satellites)
+    ts_input = interp.add_nan(ts_input)
     ts_gapfill = interp.iter_fill(ts_input, max_gap=5)
 
     # either calc climatology, apply moving average filter, or do nothing
@@ -37,7 +38,8 @@ def prepare(gpi, start_date, end_date, models, satellites, kind="clim"):
         ts_smooth = smooth.iter_climats(ts_gapfill)
         plot_title = 'Climatology'
     elif kind == 'movav':
-        ts_smooth = smooth.iter_movav(ts_gapfill, 35)
+        ts_smooth = smooth.iter_movav(ts_gapfill, window)
+        #ts_smooth = ts_gapfill
         plot_title = 'Moving average'
     elif kind == None:
         # return original data
@@ -54,23 +56,10 @@ def prepare(gpi, start_date, end_date, models, satellites, kind="clim"):
 
 
 if __name__ == "__main__":
+    import pandas as pd
+
     import matplotlib.pyplot as plt
     import matplotlib
-    matplotlib.style.use('ggplot')
+    matplotlib.style.use(['ggplot', 'seaborn-poster'])
 
-    # test
-    gpi = 726120
-
-    ts = prepare(gpi=gpi,
-            start_date='2007-01-01',
-            end_date='2016-12-31',
-            models=['merra'],
-            satellites=['ascat_reckless_rom'],
-            kind='clim')
-    print ts
-    title = 'Gpi #{}'.format(str(gpi))
-    ax = ts.plot(title=title)
-    ax.set_ylabel(r"Soil moisture ($m^{3}/m^{3}$)")
-    ax.set_xlabel(r"Time")
-
-    plt.show()
+    pass
