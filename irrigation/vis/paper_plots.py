62611557012a310630380db9aaf525047d3fc959
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2017-08-14 12:03:06 +0200

    added plotting routines

--- irrigation/vis/paper_plots.py
+++ irrigation/vis/paper_plots.py
@@ -8,11 +8,12 @@ from irrigation.comp import slopes
 from irrigation.prep import interp
 
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
 import matplotlib
 matplotlib.style.use(['ggplot', 'seaborn-poster'])
 
 
-def plot_climat_corr(gpi, location=''):
+def plot_climat_corr(gpi, location='', show=True):
     """
     Plots ASCAT and MERRA SM climatologies with a 35 day rolling correlation
     beneath indicating the general agreement between the two trends.
@@ -44,7 +45,7 @@ def plot_climat_corr(gpi, location=''):
                                                           lat=str(lat),
                                                           lon=str(lon))
     # first row
-    ts['MERRA'].plot(ax=ax1, label='MERRA', title=title)  # , ylim=(0.2,0.4))
+    ts['MERRA'].plot(ax=ax1, label='MERRA', title=title, ylim=(0.15,0.4))
     ts['ASCAT'].plot(ax=ax1, label='ASCAT')
     ax1.legend(loc=3)
     ax1.set_ylabel(r"Soil moisture ($m^{3}/m^{3}$)")
@@ -56,9 +57,12 @@ def plot_climat_corr(gpi, location=''):
 
     # plot
     plt.tight_layout()
-    plt.show()
+    if show:
+        plt.show()
+    else:
+        pass
 
-def plot_movav_corr(gpi, location=''):
+def plot_movav_corr(gpi, location='', show=True):
     """
     Plots moving averaged satellite and model soil moisture timeseries with a
     rolling correlation (35 day window).
@@ -112,10 +116,12 @@ def plot_movav_corr(gpi, location=''):
 
     # plot
     plt.tight_layout()
-    plt.show()
+    if show:
+        plt.show()
+    else:
+        pass
 
-
-def climat_slopes_subplot(gpi, location):
+def climat_slopes_subplot(gpi, location='', show=True):
     """
     Plots ASCAT and MERRA sm climatologies with daily psd.
     :param gpi:
@@ -148,11 +154,20 @@ def climat_slopes_subplot(gpi, location):
                                                           lon=str(lon))
 
     # first row
-    climat.plot(ax=ax1, title=title)
+    xticks = [1, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 366]
+    climat.plot(ax=ax1, title=title, xticks=xticks, ylim=(0,0.4))
     ax1.legend(loc=3)
     ax1.set_ylabel(r"Soil moisture ($m^{3}/m^{3}$)")
+    ax1.get_yaxis().set_label_coords(-0.1, 0.5)
     # second row
-    pos_slope_diffs.plot(ax=ax2, kind='line', legend=False, color='darkgrey')
+    pos_slope_diffs.plot(ax=ax2, kind='line', legend=False, color='black', ylim=(0,0.0065))
+    # sum for May-September (leap year doy calendar)
+    yearly_psds = pos_slope_diffs[122:274].sum()['ASCAT']
+    # add to plot as textbox
+    textstr = "PSDS = {:2.3f}".format(yearly_psds) + r" $d^{-1}$"
+    anchored_text = AnchoredText(textstr, loc=2, frameon=True, prop=dict(size=12))
+    anchored_text.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+    ax2.add_artist(anchored_text)
 
     # fill area under slopes curve
     ax2.fill_between(pos_slope_diffs.index.values, 0,
@@ -160,12 +175,16 @@ def climat_slopes_subplot(gpi, location):
                      facecolors='darkgrey')
     ax2.set_ylabel(r"PSD ($d^{-1}$)")
     ax2.set_xlabel("DOY")
+    ax2.get_yaxis().set_label_coords(-0.1, 0.5)
 
     # plot
     plt.tight_layout()
-    plt.show()
+    if show:
+        plt.show()
+    else:
+        pass
 
-def climat_slopes_corr_subplot(gpi, location):
+def climat_slopes_corr_subplot(gpi, location, show=True):
     """
     Plots ASCAT and MERRA sm climatologies with daily psd.
     :param gpi:
@@ -212,6 +231,15 @@ def climat_slopes_corr_subplot(gpi, location):
 
     # second row
     pos_slope_diffs.plot(ax=ax2, kind='line', legend=False, color='black', ylim=(0, 0.008))
+    # sum for May-September (leap year doy calendar)
+    yearly_psds = pos_slope_diffs[122:274].sum()['ASCAT']
+    # add to plot as textbox
+    textstr = "PSDS = {:2.3f}".format(yearly_psds) + r" $d^{-1}$"
+    anchored_text = AnchoredText(textstr, loc=2, frameon=True,
+                                 prop=dict(size=12))
+    anchored_text.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+    ax2.add_artist(anchored_text)
+
     # fill area under slopes curve
     ax2.fill_between(pos_slope_diffs.index.values, 0,
                      pos_slope_diffs['ASCAT'].values,
@@ -227,7 +255,10 @@ def climat_slopes_corr_subplot(gpi, location):
 
     # plot
     plt.tight_layout()
-    #plt.show()
+    if show:
+        plt.show()
+    else:
+        pass
 
 
 if __name__=='__main__':
@@ -237,11 +268,20 @@ if __name__=='__main__':
                 '763461': 'Snake River Valley, Idaho',
                 '750568': 'Plains of Nebraska',
                 '701663': 'Georgia'}
+    #id = 'psd_rollingcorr_'
 
-    out_dir = '/home/fzaussin/TIMESERIES/NEW'
-    id = 'climat_corr_psd_'
+    irrig_dict = {'730442': 'Irrigated',
+                  '730443': 'Half irrigated',
+                  '730444': 'Partially irrigated',
+                  '730445': 'Rainfed'}
+    id = 'psd_comp_'
+    out_dir = '/home/fzaussin/TIMESERIES/methods/psd'
+
+
     for gpi, loc in loc_dict.iteritems():
-        climat_slopes_corr_subplot(int(gpi), loc)
-        plt.savefig(os.path.join(out_dir, id + loc),
+        print loc
+        climat_slopes_subplot(int(gpi), loc, show=False)
+
+        plt.savefig(os.path.join(out_dir, id + loc + '.pdf'),
                     format='pdf',
                     dpi=300)
