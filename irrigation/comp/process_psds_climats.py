10fe21d260e9841eeec9e9f190bcae7647587508
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2017-08-14 12:04:04 +0200

    modified climatology based slopes process to create psds output for several time periods

--- irrigation/comp/process_psds_climats.py
+++ irrigation/comp/process_psds_climats.py
@@ -3,64 +3,90 @@ import datetime
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from irrigation.prep import timeseries
 from irrigation.comp import slopes
 
 
-# define 1 model and multiple satellite datasets
-models = ['merra']
-satellites = ['ascat_reckless_rom']
+date_dict = {'Jan' : (0,31),
+             'Feb' : (31,60),
+             'Mar' : (60,91),
+             'Apr' : (91,121),
+             'May' : (121,152),
+             'Jun' : (152,182),
+             'Jul' : (182,213),
+             'Aug' : (213,244),
+             'Sep' : (244,274),
+             'Oct' : (274,305),
+             'Nov' : (305,335),
+             'Dec' : (335,366),
+             'MAM' : (60,152),
+             'JJA' : (152,244),
+             'SON' : (244,335),
+             'AMJJASO' : (91,305)}
 
-# path to gpis
-path = '/home/fzaussin/shares/users/Irrigation/Data/lookup-tables/LCMASK_rainfed_cropland_usa.csv'
-gpis_lcmask = pd.DataFrame.from_csv(path)
-gpis_lcmask = gpis_lcmask.sort_values('gpi_quarter')
-gpis = gpis_lcmask['gpi_quarter']
-total_gpis = len(gpis)
 
-# init results container
-results = []
+if __name__=='__main__':
+    # define 1 model and multiple satellite datasets
+    models = ['merra']
+    satellites = ['ascatrecklessrom']
 
-tic = time.clock()
-counter = 0
-for row in gpis_lcmask.itertuples():
-    counter += 1
+    # path to gpis
+    path = '/home/fzaussin/shares/users/Irrigation/Data/lookup-tables/LCMASK_rainfed_cropland_usa.csv'
+    gpis_lcmask = pd.DataFrame.from_csv(path)
+    gpis_lcmask = gpis_lcmask.sort_values('gpi_quarter')
+    gpis = gpis_lcmask['gpi_quarter']
+    total_gpis = len(gpis)
 
-    gpi = row[1]
-    crop_fraction = row[2]
-    print "Processing gpi #{} of {}".format(counter, total_gpis)
-    # exemplary slopes calc from climats for JJA
-    # 721798 is mississippi example gpi
-    try:
-        df = timeseries.prepare(gpi=gpi,
-                                start_date='2007-01-01',
-                                end_date='2016-12-31',
-                                models=models,
-                                satellites=satellites,
-                                kind='clim')
-    except (IOError, ValueError):
-        # skip gpi if no index for a location id can be found
-        # problem with i=282 -> gpi #737696
-        ascat = np.nan
-        continue
-    try:
-        climat_slopes = slopes.slopes_climat(df)
-        psdiffs = slopes.psd(climat_slopes)
-        # slopes for JJA climat
-        psdiffs_sum = psdiffs[151:243].sum()
-        # divide by fractional crop AREA (!)
-        ascat = np.divide(psdiffs_sum[0], (crop_fraction*25*25))
-    except ValueError:
-        # ValueError: if no data for gpi
-        # IOError: if index for location id not found (gldas problem only?)
-        ascat = np.nan
+    # init results container
+    results = {}
 
-    results.append((gpi, ascat))
-toc = time.clock()
+    tic = time.clock()
+    counter = 0
+    for row in gpis_lcmask.itertuples():
+        counter += 1
 
-df_results = pd.DataFrame(results, columns=('gpi', 'ascat'))
-df_results.to_csv('/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/NORMALIZED_AREA/merra-ascatreckrom-usa-2007-2016-jja-climat-psds.csv')
+        gpi = row[1]
+        crop_fraction = row[2]
+        print "Processing gpi #{} of {}".format(counter, total_gpis)
+        # exemplary slopes calc from climats for JJA
+        # 721798 is mississippi example gpi
+        try:
+            df = timeseries.prepare(gpi=gpi,
+                                            start_date='2007-01-01',
+                                            end_date='2016-12-31',
+                                            models=models,
+                                            satellites=satellites,
+                                            kind='clim')
+            if df.empty:
+                ascat = np.nan
+        except (IOError, ValueError):
+            # skip gpi if no index for a location id can be found
+            # problem with i=282 -> gpi #737696
+            ascat = np.nan
+        try:
+            climat_slopes = slopes.diffquot_slope_climat(df)
+            psdiffs = slopes.psd(climat_slopes)
+            psds4dates = pd.Series(index=date_dict.keys())
+            # iterate over date ranges
+            for id, date_range in date_dict.iteritems():
+                psds4dates[id] = psdiffs[date_range[0]:date_range[1]].sum()
 
-print "Elapsed time: ", str(datetime.timedelta(seconds=toc-tic))
+            # slopes for JJA climat
+            # divide by fractional crop AREA (!)
+            ascat = psds4dates
+        except ValueError:
+            # ValueError: if no data for gpi
+            # IOError: if index for location id not found (gldas problem only?)
+            ascat = np.nan
+
+        results.update({str(gpi): ascat})
+    toc = time.clock()
+
+    #print pd.concat(results.values(), axis=1)
+    resultsT = pd.concat(results, axis=1)
+    # transpose and correctly set zeros to nan
+    df_results = resultsT.transpose().replace(0, np.nan)
+    df_results.to_csv('/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/FINAL/ascat-merra-climat-based.csv')
+
+    print "Elapsed time: ", str(datetime.timedelta(seconds=toc-tic))
