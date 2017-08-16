f67427dc3c527d9545926d499d2b715cbc1ba64e
Felix Zaussinger <felix.zaussinger@geo.tuwien.ac.at>
2017-08-16 13:46:21 +0200

    added show parameters, created func for binary map based on thresh

--- irrigation/vis/plot_confusion_matrix.py
+++ irrigation/vis/plot_confusion_matrix.py
@@ -33,6 +33,9 @@ from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
+from matplotlib.colors import ListedColormap
+from irrigation.vis.spatialplot import spatial_plot_quarter_grid
+
 
 def plot_confusion_matrix(cm, classes,
                           normalize=True,
@@ -107,13 +110,11 @@ def iter_conf_matrix(merged_data, show=False):
     :return:
     """
     mirad = merged_data['irrigation'].values
-    print mirad
     psds_agg = merged_data.drop('irrigation', axis=1)
 
     cnf_matrix_dict = {}
 
     for time_agg in psds_agg:
-        print time_agg
         cnf_matrix = confusion_matrix(mirad, psds_agg[time_agg].values)
 
         # normalize and append to dict
@@ -134,7 +135,7 @@ def iter_conf_matrix(merged_data, show=False):
     else:
         return cnf_matrix_dict
 
-def cnfm_over_time(data, bbox=None, region=''):
+def cnfm_over_time_climats(data, bbox=None, region='', show=True):
     """
     Creates bar plot of confusion matrix over time for a specific region or
     the whole input data frame.
@@ -181,41 +182,149 @@ def cnfm_over_time(data, bbox=None, region=''):
         title=title, stacked=True, ylim=(0, 200), alpha=0.7)
     ax.set_ylabel('Percent (%)')
     plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-    plt.show()
+    plt.tight_layout()
+    if show:
+        plt.show()
 
+def cnfm_over_time_movav(data, bbox=None, region='', show=True):
+    """
+    Creates bar plot of confusion matrix over time for a specific region or
+    the whole input data frame.
+    :param df:
+    :param bbox: tuple
+        (min_lon, min_lat, max_lon, max_lat)
+    :param region:
+    :return:
+    """
+    if bbox is not None:
+        min_lon, min_lat, max_lon, max_lat = bbox
+        data = merged_data.loc[(merged_data.lon <= max_lon) &
+                               (merged_data.lon >= min_lon) &
+                               (merged_data.lat <= max_lat) &
+                               (merged_data.lat >= min_lat)]
 
+    data.drop(['lon', 'lat', 'gpi_quarter'], axis=1, inplace=True)
+
+    cnf_dict = iter_conf_matrix(data)
+
+    # stack confusion matrices over time in df
+    df = pd.DataFrame.from_dict(cnf_dict)
+    # transpose and rename new columns
+    cnfm_over_time = df.transpose()
+    cnfm_over_time.rename(columns={0: 'TN',
+                                   1: 'FP',
+                                   2: 'FN',
+                                   3: 'TP'}, inplace=True)
+    # reorder columns
+    cnfm_over_time = cnfm_over_time[['TP', 'FN', 'TN', 'FP']]
+
+    # transform to % between [0, 100]
+    cnfm_over_time = cnfm_over_time.multiply(100)
+
+    # create barplot with custom colors
+    title = region
+
+    # change index to only display year and month in plot
+    cnfm_over_time.index = pd.to_datetime(cnfm_over_time.index)
+    tstamps = cnfm_over_time.index
+    format_tstamps = tstamps.map(lambda t: t.strftime('%Y-%m'))
+
+    # create plot
+    ax = cnfm_over_time.plot.bar(
+        color=['#018571', '#dfc27d', '#80cdc1', '#a6611a'],
+        title=title, stacked=True, ylim=(0, 200), alpha=0.7)
+    ax.set_ylabel('Percent (%)')
+    ax.set_xticklabels(format_tstamps)
+    plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+    plt.tight_layout()
+    if show:
+        plt.show()
+
+def binary_map(path, show=True, thresh=0):
+    """Plots binary irrigation map"""
+
+    binary_psds = psds2binary(path, thresh)
+    merged_data = align_with_mirad(binary_psds)
+
+    # create plot
+    cmap_binary = ListedColormap(['#f2f2f2', '#045a8d'], 'indexed')
+
+    if show:
+        spatial_plot_quarter_grid(merged_data, title='tag', cbar=False,
+                                  cmap=cmap_binary)
+    else:
+        dir = os.path.split(path)[0]
+        fname = os.path.split(path)[1]
+        region, mod, sat = fname.split('_')[:3]
+
+        spatial_plot_quarter_grid(merged_data, title='tag', cbar=False,
+                                  cmap=cmap_binary,
+                                  path=dir,
+                                  fname='binary_{}_{}_{}'.format(thresh, mod, sat))
+
+def unwrap_path(path):
+    dir = os.path.split(path)[0]
+    fname = os.path.split(path)[1]
+    region, mod, sat = fname.split('_')[:3]
+    return region, mod, sat
 
 if __name__== '__main__':
-    #np.set_printoptions(precision=2)
-    from irrigation.vis.spatialplot import spatial_plot_quarter_grid
+    import os
+    from irrigation.vis import spatialplot
     import matplotlib
     matplotlib.style.use(['ggplot', 'seaborn-poster'])
 
-    ###
-    # subsets
-    bbox_cali = (-124.48, 32.53, -114.13, 42.01) # state
-    bbox_nebraska = (-104.0535,40.0,-95.3081,43.0013) # state
-    bbox_georgia = (-85.6051,30.3558,-80.7514,35.0008) # state
+    bboxes = {'USA': None,
+         'California': (-124.48, 32.53, -114.13, 42.01),
+         'Nebraska': (-104.05,40.00,-95.31,43.00),
+         'Georgia': (-85.61,30.36,-80.75,35.00),
+         'Mississippi': (-92.36, 31.21,-88.98,37.61)}
 
-    bbox_mississ_flood_plain = (-92.3621,31.2175,-88.9893,37.6186) # ausschnitt
-    bbox_snake_river_valley = (-117.243,41.9881,-111.0436,44.4534) # ausschnitt
-
-    # for tests only
-    bbox_northdakota = (-104.05,45.94,-96.55,49.0)
-    bbox_texas = (-106.65,25.84,-93.51,36.5)
-    bbox_arizona = (-114.8184,31.3322,-109.0452,37.0043)
-    ###
-    # make psds and mirad binary based on thresh
-    monthly_psds = '/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/FINAL/climatology-based/ascat-merra-climat-based-months.csv'
-
-    thresh = 0.04
-    binary_psds = psds2binary(monthly_psds, thresh)
-    merged_data = align_with_mirad(binary_psds)
+    path_climat = '/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/FINAL/climatology-based/ascat-merra-climat-based-seasons.csv'
 
     # plot binary maps
-    #spatial_plot_quarter_grid(merged_data, title='tag', cbrange=(0,1), cblabel='Binary irrigated area')
-
-    # create conf-matrix bar plot
-    cnfm_over_time(merged_data, bbox=bbox_snake_river_valley, region='Snake River basin '+ str(bbox_snake_river_valley))
+    binary_map(path_climat, thresh=0.08, show=True)
 
 
+
+
+    """
+    # create conf-matrix bar plot for climats
+    path_climat = '/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/FINAL/climatology-based/ascat-merra-climat-based-months.csv'
+
+    thresh = 0.04
+    binary_psds = psds2binary(path_climat, thresh)
+    merged_data = align_with_mirad(binary_psds)
+
+
+    out_dir = '/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/FINAL/conf-matrix-final/climats-classification/'
+    for region, bbox in bboxes.iteritems():
+        print region, bbox
+        cnfm_over_time_climats(merged_data, bbox, show=False,
+                             region="{} {}".format(region, str(bbox)))
+        plt.savefig(os.path.join(out_dir, region + '.png'),
+                    dpi=300,
+                    bbox_inches='tight')
+    """
+
+    """
+    # conf matrix over time for movav
+
+    temp_res = 'monthly'
+    # make psds and mirad binary based on thresh
+    path = '/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/FINAL/movav-based/monthly_merra_smapv4am_2015-01-01_2016-12-31.csv'
+
+    region, mod, sat = unwrap_path(path)
+
+    thresh = 0.04
+    binary_psds = psds2binary(path_climat, thresh)
+    merged_data = align_with_mirad(binary_psds)
+
+    out_dir = '/home/fzaussin/shares/users/Irrigation/Data/output/new-results/PAPER/FINAL/conf-matrix-final/movav-classification'
+    for region, bbox in bboxes.iteritems():
+        print region, bbox
+        cnfm_over_time_movav(merged_data, bbox, show=False,
+                             region="{} {}".format(region,str(bbox)))
+        fname = "{}_{}_{}_{}.png".format(temp_res, mod,sat,region)
+        plt.savefig(os.path.join(out_dir, fname), dpi=300, bbox_inches='tight')
+    """
